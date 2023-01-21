ccd6fb4262ca800d193b5b8e9a7dcb07489f35f6
megha <meghasaini095@gmail.com>
2023-01-21 22:49:56 +0530

    second commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -8,6 +8,7 @@ public class DemoApplication {
 
 	public static void main(String[] args) {
 		System.out.println("k");
+		System.out.println("k");
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
